1db3fecda55bc25125f2890eadc44e3f0b97bbf7
Soumya Ranjan Sahoo <srsahoo2001.official@gmail.com>
2025-03-17 14:59:44 +0530

    bst added

---
+++ binarysearchtree/binarysearchtree.js
@@ -0,0 +1,80 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        let node = new Node(value);
+
+        if(this.root == null) {
+            this.root = node;
+            return;
+        }
+
+        let curr = this.root;
+
+        while(curr !== null) {
+            // duplicates not allowed to be inserted
+            if(node.value === curr.value) {
+                break;
+            }
+
+            if(node.value < curr.value) {
+                if(curr.left !== null) {
+                    curr = curr.left;
+                    continue;
+                }
+
+                curr.left = node;
+                break;
+            }
+
+            if(node.value > curr.value) {
+                if(curr.right !== null) {
+                    curr = curr.right;
+                    continue;
+                }
+
+                curr.right = node;
+                break;
+            }
+        }
+    }
+
+    // only for quick glance
+    print() {
+        return this.root;
+    }
+
+    search(value) {
+        if(this.root === null) {
+            console.log("BST is empty");
+            return -1;
+        }
+
+        let curr = this.root;
+        while(curr !== null) {
+            if(value === curr.value) {
+                return curr;
+            }
+            
+            if(value < curr.value) {
+                curr = curr.left;
+            } else {
+                curr = curr.right;
+            }
+        }
+
+        return -1;
+    }
+}
+
+module.exports = BinarySearchTree;
---
+++ binarysearchtree/bst-test.js
@@ -0,0 +1,18 @@
+const BinarySearchTree = require("./binarysearchtree");
+
+let bst = new BinarySearchTree();
+console.log(bst.search(20));
+bst.insert(50);
+bst.insert(30);
+bst.insert(20);
+bst.insert(35);
+bst.insert(30);
+bst.insert(70);
+bst.insert(65);
+
+console.log(bst.print());
+
+console.log("30: ", bst.search(30));
+console.log("90: ", bst.search(90));
+console.log("50: ", bst.search(50));
+console.log("70: ", bst.search(70));
